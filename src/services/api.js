6b9cfbcb07fb4c3e6477ec32217593268e388e85
Lydos <lydossky@gmail.com>
2025-03-29 22:27:26 +0300

    feat: http methods module

---
+++ src/services/api.js
@@ -0,0 +1,56 @@
+const baseURL = "http://localhost:3000";
+
+function handleResponse(response) {
+  return response.json();
+}
+
+function getHeaders(includeAuth = true) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  if (includeAuth) {
+    const token = localStorage.getItem("token");
+    if (token) headers.Authorization = `Bearer ${token}`;
+  }
+  return headers;
+}
+
+function get(url, param, includeAuth = true) {
+  const fullURL = `${baseURL}${url}/${param}`;
+
+  return fetch(fullURL, {
+    method: "GET",
+    headers: getHeaders(includeAuth),
+  }).then(handleResponse);
+}
+
+function post(url, data = {}, includeAuth = true) {
+  const fullURL = `${baseURL}${url}`;
+
+  return fetch(fullURL, {
+    method: "POST",
+    headers: getHeaders(includeAuth),
+    body: JSON.stringify(data),
+  }).then(handleResponse);
+}
+
+function put(url, data = {}, includeAuth = true) {
+  const fullURL = `${baseURL}${url}`;
+
+  return fetch(fullURL, {
+    method: "PUT",
+    headers: getHeaders(includeAuth),
+    body: JSON.stringify(data),
+  }).then(handleResponse);
+}
+
+function del(url, includeAuth = true) {
+  const fullURL = `${baseURL}${url}`;
+
+  return fetch(fullURL, {
+    method: "DELETE",
+    headers: getHeaders(includeAuth),
+  }).then(handleResponse);
+}
+
+export default { get, post, put, del };
