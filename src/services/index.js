ba115232eac5b72049ed72b1efdccb84e8a9f25f
Lydos <lydossky@gmail.com>
2025-06-19 16:43:11 +0300

    feat: exit point for services

---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+import authService from "./authService";
+import postService from "./postService";
+import userService from "./userService";
+import commentService from "./commentService";
+
+export default { authService, postService, userService, commentService };
